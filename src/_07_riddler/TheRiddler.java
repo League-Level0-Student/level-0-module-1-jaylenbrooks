baa934e0cc58f5ab0e65a7cbc44715f1691f9dd3
ilabstaff <ilabstaff@mxtechimac82460.local>
2019-07-08 17:56:45 -0700

    riddler

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,13 +12,16 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+			String ans =	JOptionPane.showInputDialog("Brothers and sisters, I have none \r\n" + 
+						"But this mans father is my fathers son.\r\n" + 
+						"Who is the man?");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		score++; String ans1 = JOptionPane.showConfirmDialog ("correct!");
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+				String ans2 =("My son");
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
